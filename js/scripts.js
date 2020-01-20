ecc2a222c547aa1e486380488175d6d914b4591f
Veronika <vera3@inbox.ru>
2020-01-20 11:55:13 +0200

    changes made

--- js/scripts.js
+++ js/scripts.js
@@ -1,36 +1,36 @@
-console.log('That was it');
+console.log("That was it");
 
-function increaseValue(){
-    var grade = document.getElementById('grade').innerText;
-    var formattedGrade = parseInt(grade);
+function increaseValue() {
+  var grade = document.getElementById("grade").innerText;
+  var formattedGrade = parseInt(grade);
 
-    if (formattedGrade < 98) {
-        document.getElementById('grade').innerText = formattedGrade + 1;
-    }
+  if (formattedGrade > 100) {
+    document.getElementById("grade").innerText = formattedGrade + 1;
+  }
 }
 
-function decreaseValue(){
-    var grade = document.getElementById('grade').innerText;
-    var formattedGrade = parseInt(grade);
+function decreaseValue() {
+  var grade = document.getElementById("grade").innerText;
+  var formattedGrade = parseInt(grade);
 
-    if (formattedGrade > 50) {
-        document.getElementById('grade').innerText = formattedGrade - 1 + '%';
-    }
+  if (formattedGrade > 0) {
+    document.getElementById("grade").innerText = formattedGrade = "0" + "%";
+  }
 }
 
 function changeColor() {
-    document.getElementById('coloredRow').style.backgroundColor = getRandomColor();
+  document.getElementById("newColor").style.backgroundColor = getRandomColor();
 }
 
 function changeBackgroundColor() {
-    document.body.style.backgroundColor = getRandomColor();
+  document.body.style.backgroundColor = getRandomColor();
 }
 
 function getRandomColor() {
-    var letters = '0123456789ABCDEF';
-    var color = '#';
-    for (var i = 0; i < 6; i++) {
-      color += letters[Math.floor(Math.random() * 16)];
-    }
-    return color;
+  var letters = "0123456789ABCDEF";
+  var color = "#";
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
 }
